e2fe6212b4f4b01977011875fc14be805d837c8c
AME-Alex <alexmesparza05@gmail.com>
2026-04-26 13:35:12 -0400

    Added database models, create and add devices

--- netmanagement/models.py
+++ netmanagement/models.py
@@ -1,3 +1,35 @@
 from django.db import models
 
 # Create your models here.
+from django.db import models
+
+class Device(models.Model):
+    DEVICE_TYPES = [
+        ('router', 'Router'),
+        ('switch', 'Switch'),
+        ('end', 'End Device'),
+    ]
+
+    NETWORK_TYPES = [
+        ('employee', 'Employee'),
+        ('management', 'Management'),
+    ]
+
+    STATUS_TYPES = [
+        ('online', 'Online'),
+        ('offline', 'Offline'),
+        ('warning', 'Warning'),
+    ]
+
+    name = models.CharField(max_length=100)
+    device_type = models.CharField(max_length=10, choices=DEVICE_TYPES)
+    network = models.CharField(max_length=15, choices=NETWORK_TYPES)
+
+    ip_address = models.GenericIPAddressField()
+
+    status = models.CharField(max_length=10, choices=STATUS_TYPES)
+    ssh_enabled = models.BooleanField(default=False)
+    firewall_enabled = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.name
--- netmanagement/views.py
+++ netmanagement/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from .models import Device
 
 def home(request):
     return render(request, 'home.html')
@@ -6,157 +7,41 @@ def home(request):
 def employee_network(request):
     active_tab = request.GET.get('tab', 'routers')
 
-    routers = [
-        {
-            'type': 'Router',
-            'name': 'EMP-RTR-1',
-            'status': 'Online',
-            'ip_address': '192.168.7.192',
-            'ssh': 'Enabled',
-            'firewall': 'Enabled',
-            'status_color': 'green',
-            'icon': 'fa-wifi',
-        },
-        {
-            'type': 'Router',
-            'name': 'EMP-RTR-2',
-            'status': 'Offline',
-            'ip_address': '192.168.171.174',
-            'ssh': 'Disabled',
-            'firewall': 'Enabled',
-            'status_color': 'red',
-            'icon': 'fa-wifi',
-        },
-    ]
-
-    switches = [
-        {
-            'type': 'Switch',
-            'name': 'EMP-SW-1',
-            'status': 'Online',
-            'ip_address': '192.168.45.135',
-            'ssh': 'Enabled',
-            'status_color': 'green',
-            'icon': 'fa-server',
-        },
-        {
-            'type': 'Switch',
-            'name': 'EMP-SW-2',
-            'status': 'Offline',
-            'ip_address': '192.168.153.105',
-            'ssh': 'Enabled',
-            'status_color': 'red',
-            'icon': 'fa-server',
-        },
-        {
-            'type': 'Switch',
-            'name': 'EMP-SW-3',
-            'status': 'Online',
-            'ip_address': '192.168.44.110',
-            'ssh': 'Disabled',
-            'status_color': 'green',
-            'icon': 'fa-server',
-        },
-        {
-            'type': 'Switch',
-            'name': 'EMP-SW-4',
-            'status': 'Online',
-            'ip_address': '192.168.134.163',
-            'ssh': 'Disabled',
-            'status_color': 'green',
-            'icon': 'fa-server',
-        },
-        {
-            'type': 'Switch',
-            'name': 'EMP-SW-5',
-            'status': 'Online',
-            'ip_address': '192.168.192.38',
-            'ssh': 'Enabled',
-            'status_color': 'green',
-            'icon': 'fa-server',
-        },
-    ]
-
-    end_devices = [
-        {
-            'type': 'End Device',
-            'name': 'EMP-PC-1',
-            'status': 'Online',
-            'ip_address': '192.168.20.12',
-            'ssh': 'N/A',
-            'status_color': 'green',
-            'icon': 'fa-laptop',
-        },
-        {
-            'type': 'End Device',
-            'name': 'EMP-PC-2',
-            'status': 'Offline',
-            'ip_address': '192.168.20.18',
-            'ssh': 'N/A',
-            'status_color': 'red',
-            'icon': 'fa-laptop',
-        },
-        {
-            'type': 'End Device',
-            'name': 'EMP-PC-3',
-            'status': 'Warning',
-            'ip_address': '192.168.20.27',
-            'ssh': 'Enabled',
-            'status_color': 'yellow',
-            'icon': 'fa-laptop',
-        },
-    ]
-
     if active_tab == 'switches':
-        devices = switches
-        page_title = 'Employee Network'
-        section_title = 'Switches'
-        total_label = f'Total Switches: {len(switches)}'
-        active_tab_name = 'switches'
+        devices = Device.objects.filter(network='employee', device_type='switch')
+        total_label = f'Total Switches: {devices.count()}'
     elif active_tab == 'end-devices':
-        devices = end_devices
-        page_title = 'Employee Network'
-        section_title = 'End Devices'
-        total_label = f'Total End Devices: {len(end_devices)}'
-        active_tab_name = 'end-devices'
+        devices = Device.objects.filter(network='employee', device_type='end')
+        total_label = f'Total End Devices: {devices.count()}'
     else:
-        devices = routers
-        page_title = 'Employee Network'
-        section_title = 'Routers'
-        total_label = f'Total Routers: {len(routers)}'
-        active_tab_name = 'routers'
+        devices = Device.objects.filter(network='employee', device_type='router')
+        total_label = f'Total Routers: {devices.count()}'
+        active_tab = 'routers'
 
     return render(request, 'employee_network.html', {
-        'page_title': page_title,
-        'section_title': section_title,
-        'total_label': total_label,
         'devices': devices,
-        'active_tab': active_tab_name,
+        'total_label': total_label,
+        'active_tab': active_tab,
+        'page_title': 'Employee Network',
     })
 
 def management_network(request):
     active_tab = request.GET.get('tab', 'routers')
 
-    routers = []
-    switches = []
-    end_devices = []
-
     if active_tab == 'switches':
-        devices = switches
-        total_label = f'Total Switches: {len(switches)}'
-        active_tab_name = 'switches'
+        devices = Device.objects.filter(network='management', device_type='switch')
+        total_label = f'Total Switches: {devices.count()}'
     elif active_tab == 'end-devices':
-        devices = end_devices
-        total_label = f'Total End Devices: {len(end_devices)}'
-        active_tab_name = 'end-devices'
+        devices = Device.objects.filter(network='management', device_type='end')
+        total_label = f'Total End Devices: {devices.count()}'
     else:
-        devices = routers
-        total_label = f'Total Routers: {len(routers)}'
-        active_tab_name = 'routers'
+        devices = Device.objects.filter(network='management', device_type='router')
+        total_label = f'Total Routers: {devices.count()}'
+        active_tab = 'routers'
 
     return render(request, 'management_network.html', {
-        'page_title': 'Management Network',
         'devices': devices,
         'total_label': total_label,
-        'active_tab': active_tab_name,
+        'active_tab': active_tab,
+        'page_title': 'Management Network',
     })
